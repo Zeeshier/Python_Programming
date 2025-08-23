fd51798e5a492dc64e11c41375e8e6940b449b67
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-08-24 01:09:06 +0500

    pydantic basemodel

--- FastAPI/2_pydantic/main.py
+++ FastAPI/2_pydantic/main.py
@@ -3,10 +3,19 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class MessageInput(BaseModel):
+    role : str
+    content : str
+
 class ChatModel(BaseModel):
-    message : str
+    messages : list[MessageInput]
 
 @app.post("/chat")
 def chat(data : ChatModel):
-    print("[+] data received " , data)
+    print("[+] ChatInput Type: " , type(data.model_dump()))
+    print("[+] ChatInput: " , data)
+    print("[+] Messages Type: " , type(data.messages))
+    print("[+] Messages: " , data.messages)
+
+
     return {"message" : "Data Validation with Pydantic"}
